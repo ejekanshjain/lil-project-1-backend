d24dff0ea65493112b3b62072d8e64d8f8b30270
Prernapaliwal21 <prernapaliwal224@gmail.com>
2020-07-09 15:07:37 +0530

    login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,49 @@
 const express = require('express')
-
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('LOL')
+router.post('/posts',verifyToken,(res,req)=>{
+    jwt.verify(req.token,'secretkey',(err,authData)=>{
+        if(err){
+            res.sendStatus(403);
+        }
+        else{
+            res.json({
+                message:'Post created..',
+                authData
+            })
+        }
+    })
+    res.json({
+
+    })
 })
+
+router.get('/', (req, res) => {
+    const user = {
+        id : 1,
+        email : 'prernapaliwal80@gmail.com',
+        password : 'prerna'
+    }
+    jwt.sign({user},'secretkey',(err,token)=>{
+        res.json({
+            token
+        })
+    })
+})
+
+function verifyToken(req,res,next){
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined'){
+        const heads = header.split(' ');
+        const headsToken = heads[1];
+        req.token = headsToken;
+        next();
+    }
+    else{
+        res.sendStatus(403);
+    }
+}
 
 module.exports = router
