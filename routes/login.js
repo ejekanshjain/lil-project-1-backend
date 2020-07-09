df9951ef084f59e8bf4c9db3b82cf9abeb974c02
Ekansh Jain <ejekanshjain@gmail.com>
2020-07-09 12:09:25 +0530

    remove shit

--- routes/login.js
+++ routes/login.js
@@ -1,40 +1,9 @@
 const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
 
-app.use(express.json())
+const router = express.Router()
 
-const users = []
-
-app.get('/users', (req, res) => {
-  res.json(users)
+router.get('/', (req, res) => {
+  res.send('LOL')
 })
 
-app.post('/users', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = { name: req.body.name, password: hashedPassword }
-    users.push(user)
-    res.status(201).send()
-  } catch {
-    res.status(500).send()
-  }
-})
-
-app.post('/users/login', async (req, res) => {
-  const user = users.find(user => user.name === req.body.name)
-  if (user == null) {
-    return res.status(400).send('Cannot find user')
-  }
-  try {
-    if(await bcrypt.compare(req.body.password, user.password)) {
-      res.send('Success')
-    } else {
-      res.send('Not Allowed')
-    }
-  } catch {
-    res.status(500).send()
-  }
-})
-
-app.listen(3000)
+module.exports = router
